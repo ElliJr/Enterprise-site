5d904c2bd08470c7d9237303bf7836e965ceb7ab
Elli junior <ellijuniorsantosdejesus8@gmail.com>
2025-05-28 10:40:14 -0300

    update site

---
+++ script.js
@@ -0,0 +1,8 @@
+function abrirMenu() {
+  document.getElementById("menuLateral").style.width = "250px";
+}
+
+/* Função para fechar o menu lateral */
+function fecharMenu() {
+  document.getElementById("menuLateral").style.width = "0";
+}
